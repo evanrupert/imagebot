6fba210e39f646628d6f472fd5db7fbad5a4476a
Evan Rupert <rupertevanr@gmail.com>
2018-09-15 15:51:13 -0400

    Added python script running

---
+++ files.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"path/filepath"
+)
+
+const imagesDir = "../images/"
+const inputFilenameBase = "input"
+
+// DownloadImage downloads an image into the images directory
+func DownloadImage(url string) (string, error)  {
+	filename := getFilename(url)
+	out, err := os.Create(filename)
+	if err != nil {
+		return "", err
+	}
+	defer out.Close()
+
+	resp, err := http.Get(url)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	_, err = io.Copy(out, resp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	return filename, nil
+}
+
+func getFilename(url string) string {
+	filename := fmt.Sprintf("%s%s", inputFilenameBase, filepath.Ext(url))
+	
+	return filepath.Join(imagesDir, filename)
+}
