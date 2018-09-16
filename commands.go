36feca5cf286af443235d7a650b8eb96eb01f261
Evan Rupert <rupertevanr@gmail.com>
2018-09-15 22:35:47 -0400

    Finishing touches

--- commands.go
+++ commands.go
@@ -24,6 +24,7 @@ func Fallback(session *discordgo.Session, msg *discordgo.MessageCreate) {
 
 // Collage runs the process to create a collage and send it back to the user
 func Collage(session *discordgo.Session, msg *discordgo.MessageCreate) {
+	removeFile(outputPath)
 	msgText := strings.ToLower(msg.Content)
 
 	filename, err := downloadMessageAttachment(msg)
@@ -87,11 +88,20 @@ func sendMessageFile(session *discordgo.Session, channelID, filename string) {
   file, err := os.Open(filename)
 
   if err != nil {
-    session.ChannelMessageSend(channelID, fmt.Sprintf("Failed to open file, Error: %v", err))
     return
   }
 
   defer file.Close()
 
   session.ChannelFileSend(channelID, filename, file)
+}
+
+func removeFile(filepath string) {
+	cmd := exec.Command("rm", filepath)
+
+	err := cmd.Run()
+
+	if err != nil {
+		fmt.Println(err)
+	}
 }
